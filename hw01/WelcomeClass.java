61e646d0991f7939fe6c07d822c2bcbcf547534e
ros316 <ros316@lehigh.edu>
2015-01-27 01:40:10 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+/////////////////////////////////////
+// Brian Chen
+// Demo @
+// Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass {
+    
+//  add main menu
+    public static void main(String[] args) {
+
+//  print the statement with box around it
+    System.out.println("--------------------");
+    System.out.println("|      WELCOME     |");
+    System.out.println("--------------------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-R--O--S--3--1--6->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("I am a Junior at Lehigh University. I am a Mathematics Major with a");
+    System.out.println("Probability and Statistics Concentration.");
+
+    //test/testtest
+        
+    }
+}
