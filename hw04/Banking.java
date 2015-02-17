2abf1ed69f3a75d535b9180b597573b64d55b458
ros316 <ros316@lehigh.edu>
2015-02-17 23:52:11 +0000

    updated Banking

--- hw04/Banking.java
+++ hw04/Banking.java
@@ -1,8 +1,8 @@
 //  Rachel Sholder
 //  CSE 02
-//  HW 04
+//  HW 04 - Banking
 
-//  comments
+//  This program uses switch statments to process transactions
 
 import java.util.Scanner;
 import java.lang.Math;
@@ -17,38 +17,41 @@ public class Banking {
         Scanner myScanner;
         myScanner=new Scanner (System.in); 
         
+        //  declare balance
         double balance = (int)(Math.random() * 400000 + 100000);
         balance /= 100;
         
+        //  pick 3 options
         System.out.print("Pick one of the following: 1- Deposit Money, 2 - Withdraw Money, 3- View Your Balance");
         
+        //  declare variables
         int number = myScanner.nextInt();
         double amount;
         
-        switch (number) {
-            case 1:
+        switch (number) {   // begin switch statement
+            case 1: // if user chooses to deposit
                 System.out.print("How much money would you like to deposit? ");
                 amount = myScanner.nextDouble();
-                if (amount < 0) {
+                if (amount < 0) {   // checks for a positive number
                     System.out.println("You did not enter a positive number.");
                 } else {
-                    balance=balance+amount;
+                    balance=balance+amount; // store new balance
                     System.out.println("New balance: $"+balance+".");
                 }
-                break;
-            case 2:
+                break;  // end case 1
+            case 2: // if user chooses to withdraw
                 System.out.print("How much money would you like to withdrawal? ");
                 amount = myScanner.nextDouble();
-                if (amount > balance) {
+                if (amount > balance) { // checks to make sure you have enough money
                     System.out.println("You cannot withdraw more money than you have.");
                 } else {
-                    balance=balance-amount;
+                    balance=balance-amount; // store new balance
                     System.out.println("New balance: $"+balance+".");
                 }
-                break;
-            case 3:
+                break;  // end case 2
+            case 3: // if user chooses to see balance
                 System.out.print("Your account balance is $"+balance+".");
-                break;
+                break;  // end case 3
         }
         
         
