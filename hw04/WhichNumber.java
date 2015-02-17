98123ac30a627f6e0af6234a6d3bf3930367cf06
ros316 <ros316@lehigh.edu>
2015-02-17 23:58:55 +0000

    updated WhichNumber

--- hw04/WhichNumber.java
+++ hw04/WhichNumber.java
@@ -7,8 +7,6 @@
 import java.util.Scanner;
 
 
-//  add comments here
-
 //  define class
 public class WhichNumber {
 
@@ -23,57 +21,78 @@ public class WhichNumber {
     
     System.out.println("Think of a number between 1 and 10 inclusive.");
     
+    // prompts the user to answer yes or no
     System.out.print("Is the number even? ");
     
-    String input = myScanner.next();
+    // assigns the user input to variable
+    String input = myScanner.next(); 
     
+    // if the number is even
     if (input.equalsIgnoreCase("Y")) {
         System.out.print("Is it divisilbe by 3? ");
         input = myScanner.next();
+        // if the number is divisible by 3
         if (input.equalsIgnoreCase("Y")) {
             number = 6;
+        // if the number is not divisible by 3
         } else if (input.equalsIgnoreCase("N")) {
             System.out.print("Is the number divislbe by 4? ");
             input = myScanner.next();
+            // if the number is divisible by 4
             if (input.equalsIgnoreCase("Y")) {
                System.out.print("Is the number divided by 4 greater than 1? ");
                input = myScanner.next();
+               // if the number divided by 4 is greater than 1
                if (input.equalsIgnoreCase("Y")) {
                    number = 8;
+                // if the number divided by 4 is not greater than 1
                } else if (input.equalsIgnoreCase("N")) {
                    number = 4;
                }
+            // if the number is not divisible by 4
             } else if (input.equalsIgnoreCase("N")) {
                 System.out.print("Is the number divisible by 5? ");
                 input = myScanner.next();
+                // if the number is divisible by 5
                 if (input.equalsIgnoreCase("Y")) {
                     number = 10;
+                // if the number is not divisible by 5
                 } else if (input.equalsIgnoreCase("N")) {
                     number = 2;
                 }
             }
         }
+        
+    // if the number is odd 
     } else if (input.equalsIgnoreCase("N")) {
         System.out.print("Is it divisible by 3? ");
         input = myScanner.next();
+        // if the number is divisble by 3
         if (input.equalsIgnoreCase("Y")) {
             System.out.print("When divided by 3 is the result greater than 1? ");
             input = myScanner.next();
+            // if the number is greater than 1 when divided by 3
             if (input.equalsIgnoreCase("Y")) {
                 number = 9;
+            // if the number is not greater than 1 when divided by 3
             } else if (input.equalsIgnoreCase("N")) {
                 number = 3;
             }
+        // if the number is not divisble by 3
         } else if (input.equalsIgnoreCase("N")) {
             System.out.print("Is it greater than 6? ");
             input=myScanner.next();
+            // if the number is greater than 6
             if (input.equalsIgnoreCase("Y")) {
                 number = 7;
+            // if the number is not greater than 6
             } else if (input.equalsIgnoreCase("N")) {
                 System.out.print("Is it less than 3? ");
                 input=myScanner.next();
+                // if the number is less than 3
                 if (input.equalsIgnoreCase("Y")) {
                     number = 1;
+                // if the number is not less than 3
                 } else if (input.equalsIgnoreCase("N")) {
                     number = 5;
                 }
@@ -82,13 +101,17 @@ public class WhichNumber {
     }
     
     if (number == 0) {
+        // print invalid input
         System.out.println("Sorry, that is not a valid input.");
     } else {
+        // ask if this is correct number
         System.out.print("Is your number " + number + "? ");
         input = myScanner.next();
         if (input.equalsIgnoreCase("Y")) {
+            // correct number
             System.out.println("Yay!");
         } else {
+            // incorrect number
             System.out.println("Noooo!");
         }
     }
